3fbcfe2d88127908d67433577a380a0d88625e53
Kevin Schoell <schoell411@gmail.com>
2019-10-01 15:49:19 -0500

    added first 2 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+const funBusLogo = document.querySelector('.logo-heading');
+
+funBusLogo.addEventListener('mouseenter', () =>{
+    funBusLogo.style.transform= 'scale(2)';
+    funBusLogo.style.transition= 'all 1s';
+})
+funBusLogo.addEventListener('mouseleave', () => {
+    funBusLogo.style.transform = 'scale(1)';
+    funBusLogo.style.transition = 'all 1s';
+})
